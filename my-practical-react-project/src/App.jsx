1e17da59a8bd91949c160f9f57495b0445831342
Blandus1 <blandine0045@gmail.com>
2025-05-21 09:56:13 -0700

    final commot

--- my-practical-react-project/src/App.jsx
+++ my-practical-react-project/src/App.jsx
@@ -1,4 +1,6 @@
+
 import Button from "./Button.jsx";
+
 
 function App() {
   
--- my-practical-react-project/src/Button.jsx
+++ my-practical-react-project/src/Button.jsx
@@ -1,9 +1,14 @@
-
+import { useState } from "react";
 function Button(){
-
+const [count,setCount] = useState(0)
     return(
         <>
-        <button type="button" className="button">Counter</button>
+        <h1>My counter app</h1>
+        <h2>Current count:{count} </h2>
+        <button onClick={()=> setCount(count+1)} className="button">Increase </button>
+        <button onClick={()=> setCount( count-1)} className="button">Decrease </button>
+
+
         </>
     );
 }
