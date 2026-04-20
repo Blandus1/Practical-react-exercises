dfb1363ad551f0115c53fb95f982cc0faa9281f2
Blandus1 <blandine0045@gmail.com>
2026-04-20 11:39:34 +0200

    removed empty context component

--- my-practical-react-project/src/App.jsx
+++ my-practical-react-project/src/App.jsx
@@ -2,7 +2,6 @@
 import {RealTimeInput } from "./realTimeInput.jsx";
 import{ Counter  }from "./counterApp.jsx";
 import Toggle from "./Toggle.jsx"
-import Context from './Context.jsx';
 import Todo from './Todo.jsx';
 import Effect from './Effect.jsx'
 import GuineaPigs from './GuineaPigs.jsx';  
@@ -15,7 +14,6 @@ function App() {
     <RealTimeInput/>
     <Toggle/>
      <Todo/>
-    <Context/>
     <Effect/>
     <GuineaPigs/>
     </>
