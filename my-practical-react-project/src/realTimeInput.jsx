c06281865a71f118ed1e1eb998cb30b3f6457314
Blandus1 <blandine0045@gmail.com>
2026-04-20 11:34:46 +0200

    Styled real time input component

--- my-practical-react-project/src/realTimeInput.jsx
+++ my-practical-react-project/src/realTimeInput.jsx
@@ -4,7 +4,7 @@ export function RealTimeInput(){
 const [text, setText]= useState('')
     return(
         <>
-        <div>
+        <div className="flex flex-col justify-center items-center gap-2 rounded-2xl p-10 bg-gray-200 w-1/2 mx-auto">
             <h1 className="font-bold">Real time input</h1>
             <input type="text" placeholder="Text..."  onChange={e=>setText(e.target.value)} className="border rounded-l shadow-2xl"/>
             <h3>The current text is: {text}</h3>
