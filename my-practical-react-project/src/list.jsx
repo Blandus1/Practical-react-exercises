cef24cd4b2484a0e4b938749a05fe6b83d51fc0d
Blandus1 <blandine0045@gmail.com>
2026-04-21 16:58:59 +0200

    List tasks done

--- my-practical-react-project/src/list.jsx
+++ my-practical-react-project/src/list.jsx
@@ -1,8 +1,11 @@
+
+
 import {useState} from 'react'
 
 export function List (){
 const [list, setList]= useState([])
 const [newList, setNewList]= useState('')
+const [toggle,setToggle]= useState(false)
 
 function handleAddToList(){
     const trimmed= newList.trim()
@@ -11,27 +14,41 @@ setList(prev=>[...prev, newList])
 setNewList('')
 }
 
+
+
     return (
         <>
         <div className="flex flex-col w-1/2 mx-auto bg-red-100 gap-3 rounded-2xl">
         <h1 className=" flex items-center justify-center font-bold  text-red-950">List items</h1>
-        <div className="flex gap-2">
-        <input type="text" placeholder="Item..." onChange={(e)=>{setNewList(e.target.value)}} className="border rounded-l  shadow-2xl "/>
-        <button onClick={handleAddToList} className="bg-blue-100 rounded-xl p-2 shadow-2xl">Add to list</button>
+        <div className="flex gap-1">
+        <input type="text" placeholder="Type item..." onChange={(e)=>{setNewList(e.target.value)}} className="rounded-xl  shadow-xl bg-white"/>
+        <button onClick={handleAddToList} className="bg-blue-100 rounded-xl p-1 shadow-2xl">Add to list</button>
+        <button onClick={()=>setToggle(!toggle)} className="bg-blue-100 rounded-xl p-1 shadow-2xl">{toggle? "Bullet": "Numbered"}</button>
         </div>
-        <ol>
+        {/*for unordered list we use list-disc list-inside */}
+        { toggle ? (
+             <ol className="list-decimal list-inside">
             {list.map((item,index)=>{
                 return (
-                    
-                    <li key={index}>{item}
-                    <button 
-                    
-                    className="text-white bg-red-400 rounded border-black"> Delete</button>
-                    </li>
+                     <li key={index} className="hover:text-red-700">{item}
+                    </li> 
                 )
-
             })}
         </ol>
+        ):(
+             <ul className="list-disc list-inside">
+            {list.map((item,index)=>{
+                return (
+                     <li key={index} className="hover:text-red-700">{item}
+                    </li> 
+                )
+            })}
+        </ul>
+        )
+        
+        
+        }
+       
         </div>
         </>
     )
